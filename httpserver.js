70839444428d21f055e654c440924f82ca0bdb1c
samuelash94 <samuelash94@gmail.com>
2015-11-18 17:25:34 -0330

    Uploaded httpserver.js

---
+++ httpserver.js
@@ -0,0 +1,121 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var path = require('path');
+
+//config
+var config = {
+    port: 8081,
+    localIPs: ['127.0.0.1'],
+    srcpath: '/src'
+};
+
+
+//creat a server
+http.createServer(processRequestRoute).listen(config.port);
+console.log("Server has started. port:"+config.port);
+
+//router URL
+function processRequestRoute(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    if (pathname === '/') {
+        pathname = "/index.html"; //default page
+    }
+    var ext = path.extname(pathname);
+    var localPath = ''; //local path
+    var staticres = false; //statict or not
+    if (ext.length > 0) {
+        localPath = '.' + pathname;
+        staticRes = true;
+    } else {
+        localPath = '.' + config.srcpath + pathname + '.js';
+        staticRes = false;
+    }
+    //do not allow remote access
+    if (config.denyAccess && config.denyAccess.length > 0) {
+        var islocal = false;
+        var remoteAddress = request.connection.remoteAddress;
+        for (var j = 0; j < config.localIPs.length; j++) {
+            if (remoteAddress === config.localIPs[j]) {
+                islocal = true;
+                break;
+            }
+        }
+        if (!islocal) {
+            for (var i = 0; i < config.denyAccess.length; i++) {
+                if (localPath === config.denyAccess[i]) {
+                    response.writeHead(403, { 'Content-Type': 'text/plain' });
+                    response.end('403:Deny access to this page');
+                    return;
+                }
+            }
+        }
+    }
+    //donot allow back ground js
+    if (staticRes && localPath.indexOf(config.srcpath) >= 0) {
+        response.writeHead(403, { 'Content-Type': 'text/plain' });
+        response.end('403:Deny access to this page');
+        return;
+    }
+
+    fs.exists(localPath, function (exists) {
+        if (exists) {
+            if (staticRes) {
+                staticResHandler(localPath, ext, response); //statict resourse
+            } else {
+                try {
+                    var handler = require(localPath);
+                    if (handler.processRequest && typeof handler.processRequest === 'function') {
+                        handler.processRequest(request, response); //dynamic resourse
+                    } else {
+                        response.writeHead(404, { 'Content-Type': 'text/plain' });
+                        response.end('404:Handle Not found');
+                    }
+                } catch (exception) {
+                    console.log('error::url:' + request.url + 'msg:' + exception);
+                    response.writeHead(500, { "Content-Type": "text/plain" });
+                    response.end("Server Error:" + exception);
+                }
+            }
+        } else { //the resourse does not exist
+            response.writeHead(404, { 'Content-Type': 'text/plain' });
+            response.end('404:File Not found');
+        }
+    });
+}
+
+//handle the dynamic resourse
+function staticResHandler(localPath, ext, response) {
+    fs.readFile(localPath, "binary", function (error, file) {
+        if (error) {
+            response.writeHead(500, { "Content-Type": "text/plain" });
+            response.end("Server Error:" + error);
+        } else {
+            response.writeHead(200, { "Content-Type": getContentTypeByExt(ext) });
+            response.end(file, "binary");
+        }
+    });
+}
+
+//the type of the Content
+function getContentTypeByExt(ext) {
+    ext = ext.toLowerCase();
+    if (ext === '.htm' || ext === '.html')
+        return 'text/html';
+    else if (ext === '.js')
+        return 'application/x-javascript';
+    else if (ext === '.css')
+        return 'text/css';
+    else if (ext === '.jpe' || ext === '.jpeg' || ext === '.jpg')
+        return 'image/jpeg';
+    else if (ext === '.png')
+        return 'image/png';
+    else if (ext === '.ico')
+        return 'image/x-icon';
+    else if (ext === '.zip')
+        return 'application/zip';
+    else if (ext === '.doc')
+        return 'application/msword';
+    else
+        return 'text/plain';
+}
