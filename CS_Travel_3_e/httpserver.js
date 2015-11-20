95418a5f56abefe31c046874953e30d31c8917ce
Osede Onodenalore <oco423@mun.ca>
2015-11-19 21:39:55 -0330

    Edited httpserver.js
    Edited httpserver.js so that it now saves your user input from the
    console into message.txt

--- CS_Travel_3_e/httpserver.js
+++ CS_Travel_3_e/httpserver.js
@@ -11,19 +11,39 @@ var config = {
 };
 
 /* Simply creates file message.txt and fills it with the given text ******************
-
  * Alternatively, use appendFile, which does the same, but also appends any new text to the end of the file
  * if it exists already.
-
 	//fs.appendFile('message.txt', 'Just now, we have created this file', function (err) {
  	fs.writeFile('message.txt', 'Just now, we have created this file', function (err) {
     if (err) throw err;
     console.log('It\'s saved! in same location.');
 });
-
 //**************************************************************************************/
 
 //create a server
+
+var readline = require('readline'),
+rl = readline.createInterface(process.stdin, process.stdout);
+
+rl.setPrompt('');
+rl.prompt();
+
+rl.on('line', function(line) {
+switch(line.trim()) {
+default:
+fs.writeFile('message.txt', line.trim(), function (err) {
+	  if (err) throw err;
+	  console.log('It\'s saved!');
+	});
+  break;
+}
+rl.prompt();
+}).on('close', function() {
+console.log('Have a great day!');
+process.exit(0);
+});
+
+
 http.createServer(processRequestRoute).listen(config.port);
 console.log("Server has started. port:"+config.port);
 
