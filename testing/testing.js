d6ed1c89b1246344f6dfaaa73209f51881b46013
samuelash94 <samuelash94@gmail.com>
2015-11-27 12:07:47 -0330

    Uploaded testing folder, for testing GET operations. Currently this code can display data from a JSON file in a desired way. It does not yet retrieve data from forms.

---
+++ testing/testing.js
@@ -0,0 +1,27 @@
+window.onload = function(){
+
+	var url = "http://localhost:8081/test.JSON";
+	var request = new XMLHttpRequest()
+
+	request.onload = function(){
+		if (request.status == 200){
+			postComment(request.responseText);
+		}
+	};
+	
+	request.open("GET", url);
+	request.send(null);
+}
+
+function postComment(responseText){
+	
+	var newReviews = document.getElementById("newReviews");
+	var reviews = JSON.parse(responseText);
+	for (var i=0; i<reviews.length; i++){
+		var review = reviews[i];
+		var div = document.createElement("div");
+		//div.setAttribute
+		div.innerHTML = review.title + ", by " + review.username;
+		newReviews.appendChild(div);
+	}
+}
